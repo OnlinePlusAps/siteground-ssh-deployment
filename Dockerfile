fe4a5acbe189b5943406d51be6b06f734b4f0a76
Andreas Lagoni <anla@onlineplus.dk>
2022-04-07 11:26:28 +0200

    siteground

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM debian:9.7-slim
 
-LABEL "com.github.actions.name"="GitHub Action for WP Engine SSH Gateway Deployment"
-LABEL "com.github.actions.description"="An action to deploy your repository to WP Engine via the SSH Gateway"
+LABEL "com.github.actions.name"="GitHub Action for Siteground SSH Gateway Deployment"
+LABEL "com.github.actions.description"="An action to deploy your repository to Siteground via the SSH Gateway"
 LABEL "com.github.actions.icon"="chevrons-up"
 LABEL "com.github.actions.color"="blue"
 
-LABEL "repository"="http://github.com/boweidev/github-action-wpengine-ssh-deploy"
-LABEL "maintainer"="Alex Zuniga <alex.zuniga@wpengine.com>"
+LABEL "repository"="https://github.com/OnlinePlusAps/siteground-ssh-deployment"
+LABEL "maintainer"="OnlinePlus <wordpress@onlineplus.dk>"
 RUN apt-get update && apt-get install -y openssh-server rsync
 ADD entrypoint.sh /entrypoint.sh
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,34 +2,34 @@
 
 set -e
 
-: ${WPE_ENV_NAME?Required environment name variable not set.}
-: ${WPE_REMOTE_PATH?Required remote path not set.}
-: ${WPENGINE_SSHG_KEY_PRIVATE?Required secret not set.}
-: ${WPENGINE_SSHG_KEY_PUBLIC?Required secret not set.}
+: ${SG_ENV_NAME?Required environment name variable not set.}
+: ${SG_REMOTE_PATH?Required remote path not set.}
+: ${SITEGROUND_SSHG_KEY_PRIVATE?Required secret not set.}
+: ${SITEGROUND_SSHG_KEY_PUBLIC?Required secret not set.}
 
 #SSH Key Vars 
 SSH_PATH="$HOME/.ssh"
 KNOWN_HOSTS_PATH="$SSH_PATH/known_hosts"
-WPENGINE_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
-WPENGINE_SSHG_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
+SITEGROUND_SSHG_KEY_PRIVATE_PATH="$SSH_PATH/github_action"
+SITEGROUND_SSHG_KEY_PUBLIC_PATH="$SSH_PATH/github_action.pub"
 
 #Deploy Vars
-WPENGINE_SSH_HOST="$WPE_ENV_NAME.ssh.wpengine.net"
-WPE_DESTINATION="$WPE_ENV_NAME"@"$WPENGINE_SSH_HOST":sites/"$WPE_ENV_NAME"/"$WPE_REMOTE_PATH"
+SITEGROUND_SSH_HOST="$SG_ENV_NAME.ssh.siteground.net"
+SG_DESTINATION="$SG_ENV_NAME"@"$SITEGROUND_SSH_HOST":sites/"$SG_ENV_NAME"/"$SG_REMOTE_PATH"
 
 # Setup our SSH Connection & use keys
 mkdir "$SSH_PATH"
-ssh-keyscan -t rsa "$WPENGINE_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
+ssh-keyscan -t rsa "$SITEGROUND_SSH_HOST" >> "$KNOWN_HOSTS_PATH"
 
 #Copy Secret Keys to container
-echo "$WPENGINE_SSHG_KEY_PRIVATE" > "$WPENGINE_SSHG_KEY_PRIVATE_PATH"
-echo "$WPENGINE_SSHG_KEY_PUBLIC" > "$WPENGINE_SSHG_KEY_PUBLIC_PATH"
+echo "$SITEGROUND_SSHG_KEY_PRIVATE" > "$SITEGROUND_SSHG_KEY_PRIVATE_PATH"
+echo "$SITEGROUND_SSHG_KEY_PUBLIC" > "$SITEGROUND_SSHG_KEY_PUBLIC_PATH"
 
 #Set Key Perms 
 chmod 700 "$SSH_PATH"
 chmod 644 "$KNOWN_HOSTS_PATH"
-chmod 600 "$WPENGINE_SSHG_KEY_PRIVATE_PATH"
-chmod 644 "$WPENGINE_SSHG_KEY_PUBLIC_PATH"
+chmod 600 "$SITEGROUND_SSHG_KEY_PRIVATE_PATH"
+chmod 644 "$SITEGROUND_SSHG_KEY_PUBLIC_PATH"
 
 # Deploy via SSH
-rsync --delete --rsh="ssh -v -p 22 -i ${WPENGINE_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" . "$WPE_DESTINATION"
+rsync --delete --rsh="ssh -v -p 22 -i ${SITEGROUND_SSHG_KEY_PRIVATE_PATH} -o StrictHostKeyChecking=no" -a --out-format="%n"  --exclude=".*" . "$SG_DESTINATION"
